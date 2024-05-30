78e9e8a2fee4d08fb184062f4fd5db4e9bd0eab0
Ivan Urra <ivan.urra.wd@gmail.com>
2024-05-30 17:12:19 +0200

    feat: new card

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Card from "@/components/card/card";
 const Home = () => {
   return (
     <div>
-      <h1>test</h1>
       <Card />
     </div>
   );
--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,11 @@
 const Header = () => {
-  <header>
-    <div className="container">
-      <h1>Headless Blog</h1>
-    </div>
-  </header>;
+  return (
+    <header>
+      <div className="container">
+        <h1>Headless Blog</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
