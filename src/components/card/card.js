831ab80f0cb80747efe1f80f60bd00dfb006de52
Ivan Urra <ivan.urra.wd@gmail.com>
2024-06-05 07:17:58 +0200

    feat: ended initial styles

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,16 +1,16 @@
 const Card = () => {
   return (
-    <div>
+    <div className="mb-8">
       <a
         href="#"
         className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-700"
       >
         <img
-          className="object-cover w-full rounded-t-lg h-96 md:h-96 md:w-1/3 md:rounded-none md:rounded-l-lg"
+          className="object-cover w-full rounded-t-lg h-96 md:h-96 md:w-1/4 md:rounded-none md:rounded-l-lg"
           src="image-4.jpg"
           alt=""
         />
-        <div className="flex flex-col justify-between p-4 leading-normal w-full md:w-2/3 ml-4">
+        <div className="flex flex-col justify-between p-4 leading-normal w-full md:w-3/4 ml-4">
           <h5 className="mb-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
             Lorem Ipsum
           </h5>
