eef1e78bea7411438441132d42e79f15541c0055
Ivan Urra <ivan.urra.wd@gmail.com>
2024-06-08 13:21:53 +0200

    refactor: props in Card

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,19 @@ import Card from "@/components/card/card";
 const Home = () => {
   return (
     <div>
-      <Card />
-      <Card />
+      <Card
+        description="Donec tempus risus eget sagittis luctus. Mauris egestas tortor eu ex finibus porta. Curabitur in sem at eros facilisis lobortis in et magna. Nullam sollicitudin congue efficitur."
+        href="/"
+        image="image-4.jpg"
+        title="Lorem Ipsum"
+      />
+
+      <Card
+        description="Donec tempus risus eget sagittis luctus. Mauris egestas tortor eu ex finibus porta. Curabitur in sem at eros facilisis lobortis in et magna. Nullam sollicitudin congue efficitur."
+        href="/"
+        image="image-4.jpg"
+        title="Lorem Ipsum"
+      />
     </div>
   );
 };
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,23 +1,21 @@
-const Card = () => {
+const Card = ({ description, href, image, title }) => {
   return (
     <div className="mb-8">
       <a
-        href="#"
+        href={href}
         className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row w-full hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-900 dark:hover:bg-gray-700"
       >
         <img
           className="object-cover w-full rounded-t-lg h-96 md:h-96 md:w-1/4 md:rounded-none md:rounded-l-lg"
-          src="image-4.jpg"
-          alt=""
+          src={image}
+          alt={title}
         />
         <div className="flex flex-col justify-between p-4 leading-normal w-full md:w-3/4 ml-4">
           <h5 className="mb-6 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Lorem Ipsum
+            {title}
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-300 md:w-2/3">
-            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
-            Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
-            Lorem Ipsum Lorem Ipsum.
+            {description}
           </p>
         </div>
       </a>
